5847feaa35f25f94f2f40f9a638bc300e3684568
jefersonlima31 <jeferssonlima4@hotmail.com>
2022-06-03 13:04:00 -0300

    Final Project

--- src/test/test/page_test.py
+++ src/test/test/page_test.py
@@ -17,23 +17,34 @@ def test_setup():
 
 
 def test_fill_success(test_setup):
+
     zipcode_id = driver.find_element(By.ID, "campoCEP")
     zipcode_id.send_keys("52040220")
     buttonElement = driver.find_element(By.XPATH, "//div[3]/fieldset/center/input[2]")
     buttonElement.click()
+    time.sleep(4)
     visibleElement = driver.find_element(By.XPATH, "//*[@id='resultado']/center/font")
+    visibleElementLogradouro = driver.find_element(By.XPATH, "//*[@id='resultado']/table/tbody/tr[2]/td[2]")
+    visibleElementDistrict = driver.find_element(By.XPATH, "//*[@id='resultado']/table/tbody/tr[3]/td[2]")
+    visibleElementCity = driver.find_element(By.XPATH, "//*[@id='resultado']/table/tbody/tr[4]/td[2]")
+    visibleElementStreet = driver.find_element(By.XPATH, "//*[@id='resultado']/table/tbody/tr[1]/td[2]")
+    visibleElementUf = driver.find_element(By.XPATH, "//*[@id='resultado']/table/tbody/tr[5]/td[2]")
 
-    print(visibleElement.is_displayed())
+    assert visibleElement.text == "sucesso - cep completo"
+    assert visibleElementStreet.text == "Rua"
+    assert visibleElementLogradouro.text == "Cirilino Afonso de Melo"
+    assert visibleElementDistrict.text == "Campo Grande"
+    assert visibleElementCity.text == "Recife"
+    assert visibleElementUf.text == "PE"
 
 
 def test_fill_fail(test_setup):
     zipcode_id = driver.find_element(By.ID, "campoCEP")
-    zipcode_id.send_keys("0")
+    zipcode_id.send_keys("0001")
     buttonElement = driver.find_element(By.XPATH, "//div[3]/fieldset/center/input[2]")
     buttonElement.click()
+    time.sleep(4)
+
     visibleElement = driver.find_element(By.XPATH, "//*[@id='resultado']/center/font")
 
-    if visibleElement.is_displayed() == False:
-        print("success")
-
-
+    assert visibleElement.text == "sucesso - cep não encontrado"
